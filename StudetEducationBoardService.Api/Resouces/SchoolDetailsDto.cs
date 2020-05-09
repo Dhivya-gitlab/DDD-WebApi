acad0d0bf984e121ed7e622001ae45b935955a2f
Dhivya-gitlab <dhivyarangaraj85@gmail.com>
2020-05-09 07:57:16 +0530

    Redis

--- StudetEducationBoardService.Api/Resouces/SchoolDetailsDto.cs
+++ StudetEducationBoardService.Api/Resouces/SchoolDetailsDto.cs
@@ -1,27 +1,29 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
 namespace StudentEducationBoardService.Api.AppModels
 {
-    public class SchoolDetailsDto :IEquatable<SchoolDetailsDto>
+    public class SchoolDetailsDto : IEquatable<SchoolDetailsDto>
     {
         public int SchoolId { get; set; }
-
+        [Required]
         public string SchoolName { get; set; }
-
+        [Required]
         public string Country { get; set; }
 
         public string CommunicationLanguage { get; set; }
+        [Required]
         public string User { get; set; }
-
+        [Required]
         public string Program { get; set; }
-
+        [Required]
         public string AssessmentPeriod { get; set; }
 
         public bool Equals([AllowNull] SchoolDetailsDto schoolDetail)
         {
             if ((schoolDetail == null) || !this.GetType().Equals(schoolDetail.GetType()))
-                { return false; }
+            { return false; }
             else
             {
                 return (SchoolId == schoolDetail.SchoolId) && (SchoolName == schoolDetail.SchoolName)
--- StudetEducationBoardService.Api/Resouces/UpdateSchoolDto.cs
+++ StudetEducationBoardService.Api/Resouces/UpdateSchoolDto.cs
@@ -5,17 +5,16 @@ namespace StudentEducationBoardService.Api.AppModels
     public class UpdateSchoolDto
     {
         [Required]
-        [MaxLength(20)]
-        [MinLength(2)]
         public string SchoolName { get; set; }
-
+        [Required]
         public string Country { get; set; }
-
+        [Required]
         public string CommunicationLanguage { get; set; }
+        [Required]
         public string User { get; set; }
-
+        [Required]
         public string Program { get; set; }
-
+        [Required]
         public string AssessmentPeriod { get; set; }
     }
 }
